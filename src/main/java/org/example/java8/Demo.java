6f123e70ee846fed31c9b5bddd40410531e264ba
raj <raj@gmail.com>
2023-01-25 16:46:45 +0530

    adding prefix and suffix

--- src/main/java/org/example/java8/Demo.java
+++ src/main/java/org/example/java8/Demo.java
@@ -4,10 +4,12 @@ import java.util.StringJoiner;
 
 public class Demo {
     public static void main(String[] args) {
-        StringJoiner stringJoiner=new StringJoiner(",");// comma as , delimiter
+        //StringJoiner stringJoiner=new StringJoiner(",");// comma as , delimiter
+        StringJoiner stringJoiner =new StringJoiner(",","[","]");
         stringJoiner.add("raja");
         stringJoiner.add("promod sir");
         stringJoiner.add("rajdhar");
         System.out.println(stringJoiner);
     }
+    // adding prefix and suffix
 }
