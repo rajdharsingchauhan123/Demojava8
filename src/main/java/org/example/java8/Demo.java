ae9153bf9851a9f161dfaad54dd6cf37e6680a60
raj <raj@gmail.com>
2023-01-25 16:50:38 +0530

    marge string joiner

--- src/main/java/org/example/java8/Demo.java
+++ src/main/java/org/example/java8/Demo.java
@@ -6,10 +6,18 @@ public class Demo {
     public static void main(String[] args) {
         //StringJoiner stringJoiner=new StringJoiner(",");// comma as , delimiter
         StringJoiner stringJoiner =new StringJoiner(",","[","]");
+        StringJoiner stringJoiner2  =new StringJoiner(",","[","]");
         stringJoiner.add("raja");
         stringJoiner.add("promod sir");
         stringJoiner.add("rajdhar");
         System.out.println(stringJoiner);
+        stringJoiner2.add("ravikant");
+        stringJoiner2.add("badal");
+        stringJoiner2.add("sivi");
+        // stringJoiner merge
+        StringJoiner merge=stringJoiner.merge(stringJoiner2);
+        System.out.println(merge);
     }
-    // adding prefix and suffix
+
+
 }
