b4d3483c7d8707878222f41651e535751c21be69
raj <raj@gmail.com>
2023-01-25 17:40:59 +0530

    lambda using consumer

--- src/main/java/org/example/java8/LambdaDemo.java
+++ src/main/java/org/example/java8/LambdaDemo.java
@@ -1,6 +1,7 @@
 package org.example.java8;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class LambdaDemo {
     public static void main(String[] args) {
@@ -11,6 +12,10 @@ public class LambdaDemo {
         arrayList.add(12);
         arrayList.add(14);
         System.out.println(arrayList);
-        arrayList.forEach(n-> System.out.println(n));
+     //   arrayList.forEach(n-> System.out.println(n));
+
+        //using consumer
+        Consumer<Integer>method=(n)-> System.out.println(n);
+        arrayList.forEach(method);
     }
 }
