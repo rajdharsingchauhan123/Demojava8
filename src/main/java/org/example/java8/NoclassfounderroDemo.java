e616c2d32a1b4743adb6e04b1d2f31c3a709a9b7
raj <raj@gmail.com>
2023-01-27 11:26:32 +0530

    noclasscasterro

---
+++ src/main/java/org/example/java8/NoclassfounderroDemo.java
@@ -0,0 +1,13 @@
+package org.example.java8;
+
+public class NoclassfounderroDemo {
+    void greetin(){
+        System.out.println("hello");
+    }
+}
+class Gfg{
+    public static void main(String[] args) {
+        NoclassfounderroDemo no=new NoclassfounderroDemo();
+        no.greetin();
+    }
+}
