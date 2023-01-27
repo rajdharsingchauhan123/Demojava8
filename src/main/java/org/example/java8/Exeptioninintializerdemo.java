4065c1f5bb0c42c4d399a33baa759f0455d83e2c
raj <raj@gmail.com>
2023-01-27 11:32:01 +0530

    exeptioninintializer demo

---
+++ src/main/java/org/example/java8/Exeptioninintializerdemo.java
@@ -0,0 +1,10 @@
+package org.example.java8;
+
+public class Exeptioninintializerdemo {
+   static int i=12/0;
+    public static void main(String[] args) {
+        System.out.println(i);
+
+
+    }
+}
