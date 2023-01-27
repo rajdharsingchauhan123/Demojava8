6b561db72515e0c480c31518984363ee3b14b4e3
raj <raj@gmail.com>
2023-01-27 10:29:09 +0530

    using try and catch the exeption

--- src/main/java/org/example/java8/ArrayoutofboundExDemo.java
+++ src/main/java/org/example/java8/ArrayoutofboundExDemo.java
@@ -3,8 +3,18 @@ package org.example.java8;
 public class ArrayoutofboundExDemo {
     public static void main(String[] args) {
         int arr[]={1,2,4,5,6};
-        for (int i=0;i<=arr.length;i++){
-            System.out.println(arr[i]);
+        int b=arr.length;
+        System.out.println(b);
+        try {
+            for (int i=0;i<=arr.length;i++){
+                System.out.println(arr[i]);
+            }
+
         }
+        catch (Exception e){
+            System.out.println("\n exeption caught");
+
+        }
+
     }
 }
