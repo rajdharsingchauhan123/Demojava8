ba13a032d7a33cd2fa264637fd25e4f01a808aad
raj <raj@gmail.com>
2023-01-27 11:20:59 +0530

    classnot found expetion

---
+++ src/main/java/org/example/java8/ClassnotfoundDemo.java
@@ -0,0 +1,19 @@
+package org.example.java8;
+
+public class ClassnotfoundDemo {
+    public static void main(String[] args) {
+        // Try block to check for exceptions
+        try {
+
+            Class.forName("GeeksForGeeks");
+        }
+
+        // Catch block to handle exceptions
+        catch (ClassNotFoundException ex) {
+
+            // Displaying exceptions on console along with
+            // line number using printStackTrace() method
+            ex.printStackTrace();
+        }
+    }
+}
