7d2ce65363d9d6e5e9b537872bc8f0196fdae01e
raj <raj@gmail.com>
2023-01-27 11:17:59 +0530

    using string then remove class cast exeption

--- src/main/java/org/example/java8/ClasscastexDemo.java
+++ src/main/java/org/example/java8/ClasscastexDemo.java
@@ -3,11 +3,13 @@ package org.example.java8;
 public class ClasscastexDemo {
     public static void main(String[] args) {
         try {
+          String s ="hello";
+          Object o =(Object)s;
 
+            //Object o =new Object();
+           // String s =(String) o;
 
-            Object o =new Object();
-            String s =(String) o;
-            System.out.println(s);
+            System.out.println(o);
         }
         catch (Exception e){
             System.out.println(e);
