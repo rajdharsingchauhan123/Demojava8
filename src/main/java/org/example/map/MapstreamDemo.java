fb7a543e346dd265239bcd13339129c121232a2f
raj <raj@gmail.com>
2023-01-30 11:29:37 +0530

    using stream sortby key

---
+++ src/main/java/org/example/map/MapstreamDemo.java
@@ -0,0 +1,16 @@
+package org.example.map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapstreamDemo {
+    public static void main(String[] args) {
+        Map<Integer,String> map=new HashMap<Integer,String>();
+        map.put(100,"Amit");
+        map.put(101,"Vijay");
+        map.put(102,"Rahul");
+        map.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
+                .forEach(System.out::println);
+    }
+}
