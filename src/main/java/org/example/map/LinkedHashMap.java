15747b14f5f37433badd023b75dae46c80d1ae6b
raj <raj@gmail.com>
2023-01-30 11:58:24 +0530

    usign linked hash map

---
+++ src/main/java/org/example/map/LinkedHashMap.java
@@ -0,0 +1,23 @@
+package org.example.map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+class LinkedHashMap1 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        // Inserting pair entries in above Map
+        // using put() method
+        map.put("vishal", 10);
+        map.put("sachin", 30);
+        map.put("vaibhav", 20);
+
+        // Iterating over Map
+        for (Map.Entry<String, Integer> e : map.entrySet())
+
+            // Printing key-value pairs
+            System.out.println(e.getKey() + " "
+                    + e.getValue());
+
+    }
+}
